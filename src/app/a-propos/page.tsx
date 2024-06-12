48823869f18f55123aec3e253d146c90dcede632
Oualter <olivier.pretre@gmx.fr>
2024-06-12 14:47:44 +0200

    typescript part 4

--- src/app/a-propos/page.tsx
+++ src/app/a-propos/page.tsx
@@ -1,22 +1,23 @@
-import BlockRendererClient from './../components/BlockRendererClient'
-
+import BlockRendererClient from '../components/BlockRendererClient'
+// import { metadata } from 'next'
 export const metadata = {
-  title: 'Mentions légales',
+  title: 'À propos',
 }
 
 const CMS_URL = process.env.CMS_URL
-const url = `${CMS_URL}/api/mentions-legales`
+const url = `${CMS_URL}/api/a-propos`
 
-export default async function mentionsLegales() {
+export default async function apropos() {
   try {
-    const response = await fetch(url)
+    const response: any = await fetch(url)
     if (response.ok) {
       const body = await response.json()
       const { Titre, Contenu } = body.data.attributes
+
       return (
-        <article className="generic-content lg:max-w-[700px] mx-auto my-6 h-full px-2 lg:px-0">
+        <article className="generic-content lg:max-w-[700px] h-full mx-auto my-6 px-2 lg:px-0">
           <h1 className="text-3xl lg:text-5xl mb-4">{Titre}</h1>
-          <BlockRendererClient content={Contenu}/>
+          <BlockRendererClient content={Contenu} />
         </article>
       )
     } else {
--- src/app/mentions-legales/page.tsx
+++ src/app/mentions-legales/page.tsx
@@ -1,21 +1,20 @@
-import BlockRendererClient from './../components/BlockRendererClient'
-// import { metadata } from 'next'
+import BlockRendererClient from '../components/BlockRendererClient'
+
 export const metadata = {
-  title: 'À propos',
+  title: 'Mentions légales',
 }
 
 const CMS_URL = process.env.CMS_URL
-const url = `${CMS_URL}/api/a-propos`
+const url = `${CMS_URL}/api/mentions-legales`
 
-export default async function apropos() {
+export default async function mentionsLegales() {
   try {
-    const response = await fetch(url)
+    const response: any = await fetch(url)
     if (response.ok) {
       const body = await response.json()
       const { Titre, Contenu } = body.data.attributes
-
       return (
-        <article className="generic-content lg:max-w-[700px] h-full mx-auto my-6 px-2 lg:px-0">
+        <article className="generic-content lg:max-w-[700px] mx-auto my-6 h-full px-2 lg:px-0">
           <h1 className="text-3xl lg:text-5xl mb-4">{Titre}</h1>
           <BlockRendererClient content={Contenu} />
         </article>
