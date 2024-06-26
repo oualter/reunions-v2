00f4d697052d7b4ee456296519cc0494daf5fe10
Oualter <olivier.pretre@gmx.fr>
2024-06-26 16:21:41 +0200

    next config modif

--- next.config.js
+++ next.config.js
@@ -29,43 +29,54 @@
 // module.exports = nextConfig
 
 module.exports = {
-  reactStrictMode: false,
-  async headers() {
-    return [
-      {
-        // matching all API routes
-        source: '/api/:path*',
-        headers: [
-          { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          { key: 'Access-Control-Allow-Origin', value: '*' },
-          {
-            key: 'Access-Control-Allow-Methods',
-            value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
-          },
-          {
-            key: 'Access-Control-Allow-Headers',
-            value:
-              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
-          },
-        ],
-      },
-    ]
-  },
+  reactStrictMode: true,
+  // async headers() {
+  //   return [
+  //     {
+  //       // matching all API routes
+  //       source: '/api/:path*',
+  //       headers: [
+  //         { key: 'Access-Control-Allow-Credentials', value: 'true' },
+  //         { key: 'Access-Control-Allow-Origin', value: '*' },
+  //         {
+  //           key: 'Access-Control-Allow-Methods',
+  //           value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+  //         },
+  //         {
+  //           key: 'Access-Control-Allow-Headers',
+  //           value:
+  //             'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+  //         },
+  //       ],
+  //     },
+  //   ]
+  // },
   images: {
     remotePatterns: [
       {
         // replace remote patterns when production
         // patterns for production just below
-        // protocol: 'https',
-        // hostname: 'strapi-reunion-pingenerator.onrender.com',
+        protocol: 'https',
+        hostname: 'strapi-reunion-pingenerator.onrender.com',
         // patterns for local dev just below
-        protocol: 'http',
-        hostname: '127.0.0.1',
-        port: '1337',
+        // protocol: 'http',
+        // hostname: '127.0.0.1',
+        // port: '1337',
         // keep line below for all environments
         pathname: '/uploads/**',
       },
     ],
     // disableStaticImages: true,
+  },
+  // experimental: {
+  //   typedRoutes: true,
+  // },
+  // output: 'export',
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    // ignoreBuildErrors: true,
   },
 }
